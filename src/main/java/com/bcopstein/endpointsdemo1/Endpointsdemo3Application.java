54158ea8867af654c3a66f65e3bf0396bf0421ad
ERIK RIBEIRO <erik.ribeiro@edu.pucrs.br>
2023-09-06 18:30:58 -0300

    first commit

---
+++ src/main/java/com/bcopstein/endpointsdemo1/Endpointsdemo3Application.java
@@ -0,0 +1,13 @@
+package com.bcopstein.endpointsdemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Endpointsdemo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Endpointsdemo3Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/bcopstein/endpointsdemo1/Livro.java
@@ -0,0 +1,4 @@
+package com.bcopstein.endpointsdemo1;
+
+public record Livro(int codigo,String titulo,String autor,int ano) {  
+}
